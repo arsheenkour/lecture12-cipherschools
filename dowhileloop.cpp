abc5971e33d758436193a610071cf04be4b1b8e1
arsheenkour <arsheenkour47@gmail.com>
2024-06-10 12:15:05 +0530

    first commit

---
+++ dowhileloop.cpp
@@ -0,0 +1,13 @@
+//do while loop first execute command and than checks the condition.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int password;
+	do
+	{
+	cin>>password;
+	}
+	while(password<999999);
+	
+}
